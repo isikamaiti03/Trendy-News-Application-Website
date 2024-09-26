4aaecae3c937ae82cc33a98b53d5befa5186a853
Isika Maiti <it2027@cemk.ac.in>
2024-09-26 18:26:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import './App.css';
+import News from "./NEWS FOLDER/News";
+
+const App = () => {
+  return (
+    <>
+      <News/>
+   </> 
+  );
+};
+
+export default App;
